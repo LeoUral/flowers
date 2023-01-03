643e2ceec14b40aef08b37ac74027fc3e553cf8d
Leonid Kudryashov <leoural66@yandex.ru>
2023-01-03 13:01:33 +0500

    admin panel

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+const { log } = require('console');
 const crypto = require("crypto");
 const ApiError = require('../error/ApiError');
 const checkSessionData = require('../model/checkSessionData');
@@ -30,31 +31,33 @@ class UserController {
         const db = req.db;
 
         try {
-            //*: проверить логин на наличие (если true - ошибка, false - регистрируем)
-            const checkLogo = await checkLogin(db, login);
+            (async () => {
+                //*: проверить логин на наличие (если true - ошибка, false - регистрируем)
+                const checkLogo = await checkLogin(db, login);
 
-            if (checkLogo) {
-                return next(ApiError.badRequest('Такой пользователь уже есть'))
-            }
+                if (checkLogo) {
+                    return next(ApiError.badRequest('Такой пользователь уже есть'))
+                }
 
-            //*: Хешировать пароль
-            const confusionPassword = crypto.createHash("sha256", SECRET).update(password).digest("hex");
+                //*: Хешировать пароль
+                const confusionPassword = crypto.createHash("sha256", SECRET).update(password).digest("hex");
 
-            //*: Сохранить весть объект в DB, collection: users
-            const data = {
-                _id: login,
-                login: login,
-                password: confusionPassword,
-                name: name,
-                surname: surname,
-                patron: patron,
-                phone: phone,
-                gender: gender
-            }
-            const resultCreateNewUser = await addNewUser(db, data); // off
+                //*: Сохранить весть объект в DB, collection: users
+                const data = {
+                    _id: login,
+                    login: login,
+                    password: confusionPassword,
+                    name: name,
+                    surname: surname,
+                    patron: patron,
+                    phone: phone,
+                    gender: gender
+                }
+                const resultCreateNewUser = await addNewUser(db, data); // off
 
-            // res.json({ 'registration': 'OK' })
-            res.json(resultCreateNewUser) //* response: {"acknowledged": true, "insertedId": "LEO"} / {"messageError": "Такой пользователь уже есть"}
+                // res.json({ 'registration': 'OK' })
+                res.json(resultCreateNewUser) //* response: {"acknowledged": true, "insertedId": "LEO"} / {"messageError": "Такой пользователь уже есть"}
+            })()
 
         } catch (err) {
             console.log(`Ошибка при регистрации: `, err);
@@ -68,28 +71,37 @@ class UserController {
      * @param {*} res 
      */
     async login(req, res, next) {
-        const { login, password } = req.body;
-        const db = req.db;
+        try {
+            (async () => {
+                const { login, password } = req.body;
+                const db = req.db;
 
-        //*: получить данные пользователя по логину
-        const userData = await getUserData(db, login);
+                //*: получить данные пользователя по логину
+                const userData = await getUserData(db, login);
 
-        //*: хешировать введенный пароль
-        const confusionPassword = crypto.createHash("sha256", SECRET).update(password).digest("hex");
+                //*: хешировать введенный пароль
+                const confusionPassword = crypto.createHash("sha256", SECRET).update(password).digest("hex");
 
-        //*: сравнить пароли
-        if (userData && userData.password === confusionPassword && userData.login === login) {
+                //*: сравнить пароли
+                if (userData && userData.password === confusionPassword && userData.login === login) {
 
-            //todo: если сходятся пароли и логин, то созадть сессию
-            console.log(`Вход в систему, созадть сессию`);
-            const sessionId = await createSession(db, login);
+                    //todo: если сходятся пароли и логин, то созадть сессию
+                    console.log(`Вход в систему, созадть сессию`);
+                    const sessionId = await createSession(db, login);
 
-            res.json({ sessionId: sessionId })
+                    res.json({ sessionId: sessionId })
 
-        } else {
-            console.log(`НЕТ ВХОДА`);
+                } else {
+                    console.log(`НЕТ ВХОДА`);
+                    return next(ApiError.badRequest('Нет входа'))
+                }
+            })()
+        } catch (err) {
+            console.log(`НЕТ ВХОДА::: `, err);
             return next(ApiError.badRequest('Нет входа'))
         }
+
+
 
     }
 
@@ -101,15 +113,22 @@ class UserController {
      * @return {void} 
      */
     async logOut(req, res, next) {
-        const { login } = req.body
-        const db = req.db;
+        try {
+            (async () => {
+                const { login } = req.body
+                const db = req.db;
 
-        console.log(login); // test
-        const resultDeleteSession = await deleteSession(db, login);
+                console.log(login); // test
+                const resultDeleteSession = await deleteSession(db, login);
 
-        if (resultDeleteSession.modifiedCount) {
-            res.json({ session: 'Сессия удалена' })
-        } else {
+                if (resultDeleteSession.modifiedCount) {
+                    res.json({ session: 'Сессия удалена' })
+                } else {
+                    return next(ApiError.badRequest('Удаление сессии. Нет сессии или логина.'))
+                }
+            })()
+        } catch (err) {
+            console.log(err);
             return next(ApiError.badRequest('Удаление сессии. Нет сессии или логина.'))
         }
     }
@@ -121,18 +140,51 @@ class UserController {
      * @param {*} next 
      */
     async check(req, res, next) {
-        const { sessionId, login } = req.query
-        const db = req.db
+        try {
+            (async () => {
+                const { sessionId, login } = req.query
+                const db = req.db
 
-        if (!sessionId || !login) {
-            return next(ApiError.badRequest('Нет сессии или логина.'))
+                if (!sessionId || !login) {
+                    return next(ApiError.badRequest('Нет сессии или логина.'))
+                }
+
+                // проверяем пользователя на наличие действующей сессси
+                const result = await checkSessionData(db, login, sessionId)
+                console.log(`RESULT SESSION::: `, result); // test
+
+                res.json({ session: result })
+            })()
+        } catch (err) {
+            console.log(`Ошибка проверки на действующую сессию: `, err);
+            return next(ApiError.badRequest('Ошибка проверки действующей сессии'))
         }
 
-        // проверяем пользователя на наличие действующей сессси
-        const result = await checkSessionData(db, login, sessionId)
-        console.log(`RESULT SESSION::: `, result); // test
 
-        res.json({ session: result })
+    }
+
+    /**
+     * Получаем всех клиентов из DB
+     * @param {*} req 
+     * @param {*} res 
+     * @param {*} next 
+     */
+    async clients(req, res, next) {
+        try {
+            (async () => {
+                const db = req.db;
+
+                const result = await db.collection('users')
+                    .find({}, { password: false }).toArray();
+
+                const arrClients = result.filter(item => item._id !== 'sessions')
+
+                res.json({ clients: arrClients });
+            })()
+        } catch (err) {
+            console.log(`Ошибка при получении списка клиентов: `, err);
+            return next(ApiError.badRequest('Ошибка получения списка клиентов'))
+        }
     }
 
 }
--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -2,6 +2,7 @@ const Router = require('express');
 const router = new Router();
 const userController = require('../controllers/userController');
 
+router.post('/clients', userController.clients)
 router.post('/registration', userController.registration)
 router.post('/login', userController.login)
 router.post('/logout', userController.logOut)
